5e51c5c86656d9d9d0eef06f0d4147b2e9e1d060
briankipchirchir <kbrianngetich@gmail.com>
2024-07-21 17:59:08 +0300

    k

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Example tasks (replace with actual tasks from indexTest.js)
+
+
+document.querySelector('main#main').remove();
+
+
+;
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Brian is the champion"; 
+
+
+
+
